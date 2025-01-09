416df3e47b53022437c9611b4e1de5e3a7aa03fb
ibnuhami <aufafawwaz12@gmail.com>
2025-01-09 13:59:49 +0700

    Add navbar component for improved site navigation

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,9 @@
+<navbar>
+    <div class="flex items-center justify-between p-6">
+        <a href="/" class="text-xl font-semibold text-black">E Commerce App</a>
+        <div class="flex items-center gap-6">
+            <a href="/login" class="text-sm font-semibold text-black">Login</a>
+            <a href="/register" class="text-sm font-semibold text-black">Register</a>
+        </div>
+    </div>
+</navbar>
